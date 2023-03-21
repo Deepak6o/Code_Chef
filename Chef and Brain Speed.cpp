23a4efcb4205bd04d5e0051fb740241446a6719f
Deepak kumar <87894208+Deepak6o@users.noreply.github.com>
2023-03-21 22:29:17 +0530

    Add files via upload

---
+++ Chef and Brain Speed.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    if(y>x)
+    {
+        cout<<"Yes";
+    }
+    else
+    {
+        cout<<"No";
+    }
+}
