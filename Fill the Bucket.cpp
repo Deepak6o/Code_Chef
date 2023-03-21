958a44923ad8270ead4c71a78720f326f74e31ed
Deepak kumar <87894208+Deepak6o@users.noreply.github.com>
2023-03-21 22:31:25 +0530

    Add files via upload

---
+++ Fill the Bucket.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for(int j=0;j<t;j++)
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<a-b<<endl;
+
+    }
+}
